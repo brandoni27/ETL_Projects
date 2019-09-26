00815d631e51b06e92f189a77ca428ed90006005
brandoni27 <brandoni27@users.noreply.github.com>
2019-09-26 16:31:59 -0700

    2nd Version of the Webscraper

--- WebScraper.py
+++ WebScraper.py
@@ -4,35 +4,62 @@ from selenium.webdriver.common.keys import Keys
 import pandas as pd
 import time
 import selenium.common.exceptions
+import openpyxl
 
 '''
-The AddressCannon 20 second throttling method works. 4 seconds is too fast.
-Need to figure out how to auto fit the length of the list.
 
-If selenium.common.exceptions.UnexpectedAlertPresentException is triggered, 
-store the last query number and start at that query after a break.
-..
+Notes and To-dos
+
+*Throttling can cause false data because the alert prevents the page from refreshing.
+*Figure out how to auto fit the length of the list.
+*If selenium.common.exceptions.UnexpectedAlertPresentException is triggered, 
+ store the last query number and start at that query after a long break.
+
+
+    Speed: 8 Throttles at 172.
+    Speed: 7 Throttles at around 152.
+
+    *Fix 'off by one error' the first in the list should not be 'str'. It should be empty.
+    
 
 '''
 
 def AddressCannon():
     '''
-    The AddressCannon queries each student's address to see where the closest Eastside school is.
 
-    Speed: 8 Throttle:172
-    Speed:
+    The Address Cannon reads a list of addresses from a xlsx sheet and queries each student's address
+    to see where the closest Eastside school is located.
+
+    Speed: 8 Throttles at 172.
+    Speed: 7 Throttles at around 152.
+
+    *Fix 'off by one error' the first in the list should not be 'str'. It should be empty.
+
 
     '''
-    #Speed = number of seconds
-    speed = 8
-    x = 0
-    east = [str]
 
-    while x < 1000:
+
+    #Speed = number of seconds between queries.
+    speed = 7
+
+    #Holds the list of the closest Eastside School to the address.
+    east = []
+
+    #Holds IDs to load dictionary, then Dataframe.
+    dictionaryIDs = []
+
+    #Holds Addresses to load dictionary, then dataframe
+    dictionaryAddress = []
+
+    #Initiator for the while loop.
+    x = 0
+
+    while x < 3:
 
         fromBar.send_keys(AddressList[x])
         fromBar.send_keys(Keys.RETURN)
         time.sleep(speed)
+
         #print(str(AddressList[x]))
         try:
             east.append(driver.find_element_by_xpath("//*[@id='resultschools']/p").text)
@@ -40,15 +67,28 @@ def AddressCannon():
             east.append('-----')
         except selenium.common.exceptions.UnexpectedAlertPresentException:
             print("Throttling...")
-            time.sleep(60)
+            time.sleep(30)
             try:
                 east.append(driver.find_element_by_xpath("//*[@id='resultschools']/p").text)
             except selenium.common.exceptions.NoSuchElementException:
                 east.append('-----')
-        print(x, east[x], IDList[x])
+        print(x, "  ", AddressList[x], "    ", IDList[x], " ", east[x], "   ", len(east))
         time.sleep(speed)
+        dictionaryIDs.append(IDList[x])
+        dictionaryAddress.append(AddressList[x])
         driver.find_element_by_name("from").clear()
         x+=1
+
+
+
+    dataDictionary = { 'Student ID' : dictionaryIDs, 'Eastside School' : east, 'Address' : dictionaryAddress }
+
+    masterDataFrame = pd.DataFrame(dataDictionary)
+    masterDataFrame.to_excel("/Users/Brando/Desktop/Python Code/StudentDataPyDump.xlsx")
+    print(masterDataFrame)
+
+
+
 
 '''
 Importing the data from the spreadsheet I pulled from PowerSchool
@@ -57,6 +97,9 @@ Step 2: Read the WorkBook and store it.
 Step 3: Create a DataFrame using only the Student_Number and Address columns.
 Step 4: Scan the columns and add them to a list.
 '''
+#global eastsideDF
+#global addressListDF
+#global ID_DF
 
 PowerSchoolData = pd.ExcelFile("/Users/Brando/Desktop/Python Code/StudentDataPy.xlsx")
 sheet = pd.read_excel(PowerSchoolData)
@@ -68,27 +111,8 @@ IDList = frame['Student_Number'].tolist()
 driver = webdriver.Chrome()
 baseURL = "https://www.myschoollocation.com/eastsideHSD/"
 driver.get(baseURL)
+time.sleep(15)
 fromBar = driver.find_element_by_name("from")
+time.sleep(10)
 
 AddressCannon()
-
-
-
-
-
-
-
-
-
-#fromBar.send_keys("1813 Margaret Street, san jose")
-#fromBar.send_keys(Keys.RETURN)
-
-
-#You must set the number to define the size of the list.
-#This should be automated as well.
-
-
-#schoolResults = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div[2]/div/div/div/div[4]/p').text
-#print(schoolResults)
-#schoolAddress = driver.find_element_by_id()
-
